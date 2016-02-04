d8964cde23be1e28ead00f0c424721a2ed386567
zlatyyy <ziyovcheva5@gmail.com>
2016-02-05 00:44:07 +0200

    Second commit

--- com.ikea.cucumber/src/main/java/com/ikea/cucumber/Cart.java
+++ com.ikea.cucumber/src/main/java/com/ikea/cucumber/Cart.java
@@ -1,5 +1,25 @@
 package com.ikea.cucumber;
 
 public class Cart {
-
+	private String id;
+	private String quantity;
+	private String opts;
+	public String getId() {
+	return id;
+	}
+	public void setId(String id) {
+	this.id = id;
+	}
+	public String getQuantity() {
+	return quantity;
+	}
+	public void setOpts(String opts) {
+	this.opts = opts;
+	}
+	public String getOpts() {
+	return opts;
+	}
+	public void setQuantity(String opts) {
+	this.opts = opts;
+	}
 }
